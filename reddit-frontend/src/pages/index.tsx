1a9ed4689223c6e417d593fae68970776c0ebf50
sagarvadalia <sv344@cornell.edu>
2020-08-28 00:59:24 -0400

    display posts

--- reddit-frontend/src/pages/index.tsx
+++ reddit-frontend/src/pages/index.tsx
@@ -1,12 +1,18 @@
 import { withUrqlClient } from 'next-urql';
 import { Navbar } from '../components/Navbar';
 import { createURQLClient } from '../utils/createURQLClient';
+import { usePostsQuery } from '../generated/graphql';
 
-const Index = () => (
-  <>
-    <Navbar />
-    <div>Hello World</div>{' '}
-  </>
-);
+const Index = () => {
+  const [{ data }] = usePostsQuery();
+  return (
+    <>
+      <Navbar />
+      <div>Hello World</div>
+      <br />
+      {!data ? null : data.posts.map((p) => <div key={p.id}>{p.title}</div>)}
+    </>
+  );
+};
 
 export default withUrqlClient(createURQLClient)(Index);
